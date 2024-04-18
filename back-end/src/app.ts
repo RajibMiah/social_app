7fe449bfad69aebee766128de86a39f1c573888e
rajibmiah <ahmedrajib942@gmail.com>
2024-04-18 22:46:16 +0600

    router issue fixed

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -3,26 +3,32 @@ import express from "express";
 import "reflect-metadata";
 import AppDataSource from "./data-source";
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4000;
 const users = require("./routers/users");
 const posts = require("./routers/posts");
+const comments = require("./routers/comments");
+const messages = require("./routers/messages");
 const cors = require("cors");
 require("dotenv").config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
+app.use(
+  cors({
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use(cors());
 app.use("/api/users", users);
 app.use("/api/posts", posts);
 
-// app.use("/api/comments", comments);
-// app.use("/api/messages", messages);
+app.use("/api/comments", comments);
+app.use("/api/messages", messages);
 
 AppDataSource.initialize()
   .then(() => {
